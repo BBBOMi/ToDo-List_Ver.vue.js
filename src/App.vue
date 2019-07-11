b710a059d4847fb5c40cb368e78b718fa9e9eb28
bomi <a51022866@gmail.com>
2019-07-11 17:49:02 +0900

    Update content

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="todolistapp">
+    <section class="todoapp">
+      <todo-header></todo-header>
+      <section class="main">
+        <input class="toggle-all" type="checkbox">
+        <ul class="todo-list">
+          <li class="todo">
+            <div class="view">
+              <input type="checkbox" class="toggle"/>
+              <button class="destroy"></button>
+            </div>
+            <input type="text" class="edit">
+          </li>
+        </ul>
+      </section>
+      <footer class="footer">
+        <span class="todo-count">
+          <strong></strong>0 left
+        </span>
+        <ul class="filters">
+          <li><a href="#/all>">All</a></li>
+          <li><a href="#/active">Active</a></li>
+          <li><a href="#/completed">Completed</a></li>
+        </ul>
+        <button class="clear-completed">
+          Clear completed
+        </button>
+      </footer>
+    </section>
+    <footer class="info">
+      <p>Double-Click to edit a todo</p>
+      <p>Written by Bomi</p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
   </div>
 </template>
 
 <script>
+  import TodoHeader from "./components/Header.vue";
+
 export default {
-  name: 'App'
+  name: 'todo-list',
+  components: {
+    TodoHeader
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
